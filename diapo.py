9e8d3dd529d1a95a21007e88b53299f9e0a61ea5
Tom ROMAO <102719175+Avaritiainme@users.noreply.github.com>
2023-12-04 21:30:41 +0100

    Update diapo.py
    Update de nouveau liens

--- diapo.py
+++ diapo.py
@@ -5,18 +5,20 @@ import time
 browser = webdriver.Chrome()
 
 urls = [
-    'https://trello.com/b/HvvFt5zB/global-2023-2024',
-    'https://trello.com/b/6Wwo52in/communication-2023-2024',
-    'https://trello.com/b/UzTPByBA/event-2024-2024',
-    'https://trello.com/b/419eTmFG/sponsoring-2023-2024',
-    'https://trello.com/b/rDH08a0K/openlab-2023-2024',
+    'https://trello.com/b/HvvFt6zB/global-2023-2024',
+    'https://trello.com/b/7Wwo52in/communication-2023-2024',
+    'https://trello.com/b/UzTPByBA/event-2023-2024',
+    'https://trello.com/b/420eTmFG/sponsoring-2023-2024',
+    'https://trello.com/b/rDH09a0K/openlab-2023-2024',
+    'https://etesiea-my.sharepoint.com/personal/air-esiea_et_esiea_fr/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fair-esiea_et_esiea_fr%2FDocuments%2FAir%20ESIEA%2F01_Administratif%2F2023-2024%2FRèglements%2FReglementIntérieur_V5%2Epdf&parent=%2Fpersonal%2Fair-esiea_et_esiea_fr%2FDocuments%2FAir%20ESIEA%2F01_Administratif%2F2023-2024%2FRèglements',
+    
 ]
 
 try:
     while True:
         for url in urls:
             browser.get(url)
-            time.sleep(14)
+            time.sleep(60)
 
 except KeyboardInterrupt:
     print("Arret du diaporama.")
