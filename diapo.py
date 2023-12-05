3b57c71781547366eccf4577a43eaae92ec4ab6d
Tom ROMAO <102719175+Avaritiainme@users.noreply.github.com>
2023-12-05 16:10:03 +0100

    Update diapo.py
    Update des liens pour régler le problèmes d'affichage du règlement

--- diapo.py
+++ diapo.py
@@ -10,8 +10,9 @@ urls = [
     'https://trello.com/b/UzTPByBA/event-2023-2024',
     'https://trello.com/b/420eTmFG/sponsoring-2023-2024',
     'https://trello.com/b/rDH09a0K/openlab-2023-2024',
-    'https://etesiea-my.sharepoint.com/personal/air-esiea_et_esiea_fr/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fair-esiea_et_esiea_fr%2FDocuments%2FAir%20ESIEA%2F01_Administratif%2F2023-2024%2FRèglements%2FReglementIntérieur_V5%2Epdf&parent=%2Fpersonal%2Fair-esiea_et_esiea_fr%2FDocuments%2FAir%20ESIEA%2F01_Administratif%2F2023-2024%2FRèglements',
-    
+    'https://tinyurl.com/55bfradx',
+    'https://tinyurl.com/axtnw8a',
+    'https://tinyurl.com/mvf6c6an',
 ]
 
 try:
